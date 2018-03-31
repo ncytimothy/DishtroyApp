ccf7c71b26ffbb5c4d426c8878fbd638bc689a71
Timothy Ng <ncy.timothy@gmail.com>
2018-03-30 21:24:28 -0700

    Add picker view delegate and data source

--- Dishtroy/View Controllers/HomeViewController.swift
+++ Dishtroy/View Controllers/HomeViewController.swift
@@ -8,27 +8,37 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UIPickerViewDelegate {
     
     // MARK: - Properties
     
     @IBOutlet weak var foodImageView: UIImageView!
     @IBOutlet weak var forceSlider: UISlider!
     @IBOutlet weak var foodPicker: UIPickerView!
+    var pickerDataSource = ["Tomato", "Cola"]
     
     // MARK: - Life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        foodPicker.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+// MARK: - FoodPickerView Data Source
+
+extension HomeViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return pickerDataSource.count
+    }
     
-
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
 }
 
